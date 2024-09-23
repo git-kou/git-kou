625c560abe8ff2ad70cfa1f412d8b384b60f6f35
git-kou <169222307+git-kou@users.noreply.github.com>
2024-09-23 14:26:31 +0900

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 $(function() {
+
     $('.han').click(function() {
         $('.header-media').slideToggle();
         $(this).toggleClass('active');
     });
 
+    $('.header-media a').click(function() {
+        $('.header-media').slideUp();
+        $('.han').removeClass('active');
+    });
+
     $('.header-media a,main,footer').click(function() {
         $('.header-media').slideUp();
         $('.han').removeClass('active');
+    });
+
+    $(window).resize(function () {
+        if(window.matchMedia("(min-width:601px)").matches) {
+            $('.header-media').fadeOut(0);
+            $('.han').removeClass('active');
+        };
     });
 });
